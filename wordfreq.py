d12a18d45a5d4a2d7957c904e5d07632a1e892d1
Isak <skoog.isak@gmail.com>
2026-09-10 08:40:03 +0200

    fill wordfreq.py with example from assignment

--- wordfreq.py
+++ wordfreq.py
@@ -1,2 +1,19 @@
 def tokenize(lines):
-    pass
+    words = []
+    for line in lines:
+        start = 0
+        while start < len(line):
+            print(line[start])
+            start = start+1
+    return words
+
+
+
+
+def main():
+    print(tokenize(['apple', 'pie']))
+
+
+
+
+main()

59503795aa8d88b98f3175b20a39374e2b230f40
Isak <skoog.isak@gmail.com>
2026-09-15 16:46:00 +0200

    add files for lab2

---
+++ lab2/lab2/model.py
@@ -0,0 +1,28 @@
+import math
+
+# Task (2/12): Define a class Vec
+
+# Task (3/12): Additionally define a function dot(u, v)
+
+# Task (4/12): Create a class Particle
+
+# Task (5/12): In the Particle class, implement a method inertial_move(self, dt).
+
+# Task (6/12): In the Particle class, implement a method apply_force(self, dt, f)
+
+##########################################
+### NB. Tasks 7–8 are done in view.py. ###
+##########################################
+
+
+# Task (9/12): In the Particle class, add a method bounding_box(self)
+
+
+
+
+
+
+###########################################
+### When you're done with all 12 tasks: ###
+### forces/other features in this file! ###
+###########################################
---
+++ lab2/lab2/view.py
@@ -0,0 +1,13 @@
+# Task (7/12): Draw on canvas
+
+# Task (8/12): Define a new function to_canvas_coords(canvas, x)
+
+#######################################
+### NB. Task 9 is done in model.py. ###
+#######################################
+
+# Task (10/12): Define a new function move_oval_to(o, u1, u2)
+
+# Task (11/12): Define a new function create_oval(canvas, particle)
+
+# Task (12/12): Define a function simulation_loop(f, timestep, particles)

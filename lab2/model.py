e8cdc3d79a34ac0439ae713c68945805fe071231
Elias-Jadefrid <152277521+Isotop296@users.noreply.github.com>
2026-09-23 15:30:52 +0200

    update
    update

--- lab2/model.py
+++ lab2/model.py
@@ -110,6 +110,8 @@ def dot(u: Vec, v: Vec) -> float | int:
 # Task (4/12): Create a class Particle
 
 
+
+
 class Particle:
     def __init__(self, mass: int, position: Vec, velocity: int, radius: float) -> None:
         self.mass = mass
@@ -118,7 +120,8 @@ class Particle:
         self.radius = radius
         self.x, self.y = position.get_coords()
 
-    def inertial_move(self, dt) -> Vec:
+# Task (5/12): In the Particle class, implement a method inertial_move(self, dt).
+    def inertial_move(self, dt: float) -> Vec:
         """
         Modifies the position attribute according the formula:
         (t1 - t0)v + x at time t1
@@ -128,11 +131,16 @@ class Particle:
         self.position = distans_x + self.position
         return self.position
         
-    def apply_force(self, dt, f):
+
+# Task (6/12): In the Particle class, implement a method apply_force(self, dt, f)
+    def apply_force(self, dt: float, f: int):
         acceleration = f.__rmul__(self.mass)
         time_force_over_mass = dt * acceleration
         self.velocity = self.velocity + time_force_over_mass
         return self.velocity
+
+
+
 # Task (9/12): In the Particle class, add a method bounding_box(self)
 
     def bounding_box(self):
